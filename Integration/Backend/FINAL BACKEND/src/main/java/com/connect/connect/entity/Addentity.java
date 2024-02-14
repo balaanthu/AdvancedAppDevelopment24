09c46a03f39620ca4ecc43481dd38eedcb6f650d
Darmabalakanahesh A M <119047499+balaanthu@users.noreply.github.com>
2024-02-14 12:10:52 +0530

    Add files via upload

---
+++ Integration/Backend/FINAL BACKEND/src/main/java/com/connect/connect/entity/Addentity.java
@@ -0,0 +1,38 @@
+package com.connect.connect.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Addingtable")
+public class Addentity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "Coursename",nullable = false)
+    private String coursename;
+
+    @Column(name = "Duration",nullable = false)
+    private String duration;
+
+    @Column(name = "Fee",nullable = false)
+    private int fee;
+
+
+
+}
+
